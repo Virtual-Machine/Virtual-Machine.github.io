6bd53f8bf77cfe683f80c22226ce7fc7482d71e2
Virtual-Machine <darrelm@tech-asteroid.com>
2016-03-07 14:34:46 -0500

    Convert page to a chat UI example

--- js/main.js
+++ js/main.js
@@ -1,4 +1,55 @@
-function showItem(num){
-	$('#content-text').text("Displaying item: " + num);
-	$('body').toggleClass('border_green');
+var messages = [];
+var messageCount = 0;
+
+var name = prompt("What is your name?");
+if(!name){
+	name = "Anon";
 }
+
+document.getElementById('submit_text').focus();
+
+document.getElementById('curUser').innerHTML = name;
+
+document.getElementById('submit_text').onkeypress = function(event){
+	if (event.which == 13){
+		submitMessage();
+	}
+}
+
+document.getElementById('submit_button').onclick = submitMessage;
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function submitMessage(){
+	var message = {
+		name: name,
+		content: document.getElementById('submit_text').value
+	}
+	messages.push(message);
+	document.getElementById('submit_text').value = "";
+	console.log(messages);
+}
+
+function loopCheck(){
+	while(messageCount < messages.length){
+		
+		var newElem = document.createElement("div");
+		newElem.className = "message";
+		var time = new Date();
+		var timestamp = addZero(time.getHours()) + ":" + addZero(time.getMinutes()) + ":" + addZero(time.getSeconds());
+		newElem.innerHTML =  timestamp + " - " + messages[messageCount].name + " ~> " + messages[messageCount].content;
+		var topMessage = document.getElementById('content_window').firstChild;
+		document.getElementById('content_window').insertBefore(newElem, topMessage);
+		messageCount++;
+	}
+	setTimeout(function(){
+		loopCheck();
+	}, 500)
+}
+
+loopCheck();
