db1e40718c40d9380c2ba733894dce0d3781d6e2
Virtual-Machine <darrelm@tech-asteroid.com>
2016-03-07 20:59:50 -0500

    Get UI working with API server.

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@ var users = [];
 var messageCount = 0;
 
 var name = prompt("What is your name?");
-if(!name){
+if(name == "null" || name == ""){
 	name = "Anon";
 }
 
+// Submit name to data server
 $.ajax({url:'https://polar-mesa-45755.herokuapp.com/api/users',
-		type: 'GET',
+		type: 'POST',
+		data: {
+			name: name
+		},
 		crossDomain: true,
-		success: function(result){
-			users = result;
-			console.log(users);
+		success: function(){
 		}
-	})
+	});
 
 document.getElementById('submit_text').focus();
-
-document.getElementById('curUser').innerHTML = name;
 
 document.getElementById('submit_text').onkeypress = function(event){
 	if (event.which == 13){
@@ -36,30 +36,101 @@ function addZero(i) {
 }
 
 function submitMessage(){
-	var message = {
-		name: name,
-		content: document.getElementById('submit_text').value
-	}
-	messages.push(message);
+	$.ajax({url:'https://polar-mesa-45755.herokuapp.com/api/messages',
+			type: 'POST',
+			data: {
+				message: name + " ~> " + document.getElementById('submit_text').value
+			},
+			crossDomain: true,
+			success: function(){
+			}
+		});
 	document.getElementById('submit_text').value = "";
-	console.log(messages);
 }
 
 function loopCheck(){
-	while(messageCount < messages.length){
-		
-		var newElem = document.createElement("div");
-		newElem.className = "message";
-		var time = new Date();
-		var timestamp = addZero(time.getHours()) + ":" + addZero(time.getMinutes()) + ":" + addZero(time.getSeconds());
-		newElem.innerHTML =  timestamp + " - " + messages[messageCount].name + " ~> " + messages[messageCount].content;
-		var topMessage = document.getElementById('content_window').firstChild;
-		document.getElementById('content_window').insertBefore(newElem, topMessage);
-		messageCount++;
-	}
+	$.ajax({url:'https://polar-mesa-45755.herokuapp.com/api/messages',
+			type: 'GET',
+			crossDomain: true,
+			success: function(result){
+				if(messages.toString() == result.toString()){
+					return;
+				}
+				$('#content_window').empty();
+				messages = result;
+				for (var i in messages){
+					$('#content_window').prepend("<div class='message'>" + messages[i] + "</div>")
+				}
+			}
+		});
 	setTimeout(function(){
 		loopCheck();
-	}, 500)
+	}, 1000)
 }
 
 loopCheck();
+
+function pollForUsers(){
+	$.ajax({url:'https://polar-mesa-45755.herokuapp.com/api/users',
+			type: 'GET',
+			crossDomain: true,
+			success: function(result){
+				if(users.toString() == result.toString()){
+					return;
+				}
+				users = result;
+				$('#user-list').empty();
+				for(var i in users){
+					$('#user-list').append("<p>" + users[i] + "</p>")
+				}
+			}
+		})
+	setTimeout(function(){
+		pollForUsers();
+	}, 30000);
+}
+
+pollForUsers();
+
+$('#logOut')[0].onclick = function(){ removeName();}
+$('#reset')[0].onclick = function(){ serverReset();}
+
+function removeName(){
+	$.ajax({url:'https://polar-mesa-45755.herokuapp.com/api/users',
+			type: 'DELETE',
+			crossDomain: true,
+			data: {
+				name: name
+			},
+			success: function(result){
+				name = "";
+			}
+		})
+}
+
+function serverReset(){
+	$.ajax({url:'https://polar-mesa-45755.herokuapp.com/api/refresh',
+			type: 'POST',
+			crossDomain: true,
+			success: function(result){
+				$.ajax({url:'https://polar-mesa-45755.herokuapp.com/api/users',
+						type: 'POST',
+						data: {
+							name: name
+						},
+						crossDomain: true,
+						success: function(){
+						}
+					});
+			}
+		})
+}
+
+
+window.onbeforeunload = function() {
+	if (name != ""){
+		return 'You need to log out first please or this app will show you as online until the next server refresh.';	
+	} else {
+		return null;
+	}
+};
